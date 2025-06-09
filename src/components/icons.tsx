2b868c6d00093cff80f8ac2c856ae8de9b16ad94
SAAD EL AHRACH <saadjone00@gmail.com>
2025-06-09 01:52:53 +0000

    this problem again

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import type { SVGProps } from 'react';
 
 export const AppLogo = () => (
   <img
-    src="/assets/logo.png"
+    src="/assets/reboxit-logo.png"
     alt="App Logo"
     width={40}
     height={40}
